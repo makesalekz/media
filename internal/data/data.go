71b2f14ad2289862ef109c7037ec2a0bcabf9792
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2023-11-24 11:04:39 +0600

    removed debug

--- internal/data/data.go
+++ internal/data/data.go
@@ -26,18 +26,13 @@ type Data struct {
 func NewData(c *conf.Bootstrap, logger log.Logger) (*Data, func(), error) {
 	l := log.NewHelper(logger)
 
-	automigrate := os.Getenv("AUTOMIGRATE")
-	options := []ent.Option{}
-	if automigrate != "" {
-		options = append(options, ent.Debug(), ent.Log(l.Info))
-	}
-
-	client, err := ent.Open("postgres", c.Db, options...)
+	client, err := ent.Open("postgres", c.Db)
 	if err != nil {
 		l.Fatalf("failed opening connection to postgres: %v", err)
 		return nil, nil, err
 	}
 
+	automigrate := os.Getenv("AUTOMIGRATE")
 	if automigrate != "" {
 		if err := client.Schema.Create(context.Background()); err != nil {
 			l.Errorf("failed creating schema resources: %v", err)
