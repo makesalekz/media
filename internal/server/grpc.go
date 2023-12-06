e00e0cf26b94ff89e5034fc3c3f4d3cb5e228ac1
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2023-12-06 16:58:47 +0600

    changed buckets

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/metadata"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	jwtv4 "github.com/golang-jwt/jwt/v4"
@@ -27,6 +29,10 @@ func NewGRPCServer(c *conf.Bootstrap, logger log.Logger, jwtp *data.JwtProcessor
 			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
 				return jwtp.GetSecret(), nil
 			}, jwt.WithSigningMethod(jwtv4.SigningMethodHS256), jwt.WithClaims(func() jwtv4.Claims { return &jwtv4.RegisteredClaims{} })),
+			metrics.Server(
+				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
+				metrics.WithRequests(prom.NewCounter(_metricRequests)),
+			),
 		),
 		grpc.Options(ggrpc.MaxRecvMsgSize(maxRecvMsgSize)),
 	}
