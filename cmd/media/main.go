668790b7ada6c81e604298127fb9e81196c8f41d
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2023-12-14 14:12:03 +0600

    updated version for tags

--- cmd/media/main.go
+++ cmd/media/main.go
@@ -22,7 +22,7 @@ var (
 	// Name is the name of the compiled software.
 	Name string = "media"
 	// Version is the version of the compiled software.
-	Version string = "0.1.2"
+	Version string = "1.0.0"
 	// flagconf is the config flag.
 	flagconf string
 
